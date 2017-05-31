dc93f3578160833765bc44b3d5ec6ba6294c0555
Alejandro Lora <alejandrofpo@gmail.com>
2017-05-31 22:40:05 +0100

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
-    entry: './src/app.ts',
+    entry: ['./src/app/app.ts', './src/styles/style.scss'],
     output: {
         filename: 'bundle.js',
         path: __dirname + '/src'
     },
+    devtool: "source-map",
     module : {
         rules: [
             {
+                test: /\.ts$/,
                 loader: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract
+                ({
+                  fallback: 'style-loader',
+                  use: 'css-loader?sourceMap!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true'
+                }),
             }
         ]
     },
@@ -20,5 +31,8 @@ module.exports = {
         historyApiFallback: {
             index: 'src/index.html'
         }
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ]
 }
