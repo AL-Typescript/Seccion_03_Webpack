7430eacd79f7c608e235f06a827195c255b084b7
Alejandro Lora <alejandrofpo@gmail.com>
2017-06-04 01:02:47 +0100

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack');
+
+var distFolder = 'dist';
+var port = 8080;
+
 module.exports = {
     entry: ['./src/app/app.ts', './src/styles/style.scss'],
     output: {
-        filename: 'bundle.js',
-        path: __dirname + '/src'
+        filename: 'bundle.min.js',
+        path: __dirname + '/' + distFolder
     },
     devtool: "source-map",
     module : {
@@ -20,6 +28,13 @@ module.exports = {
                   fallback: 'style-loader',
                   use: 'css-loader?sourceMap!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true'
                 }),
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file-loader?name=assets/img/[name].[ext]',
+                    'image-webpack-loader'
+                ]
             }
         ]
     },
@@ -27,12 +42,29 @@ module.exports = {
         extensions: [".ts", ".js"]
     },
     devServer: {
-        port: 8080,
+        port: port,
         historyApiFallback: {
             index: 'src/index.html'
         }
     },
     plugins: [
-        new ExtractTextPlugin('bundle.css')
+        new ExtractTextPlugin('bundle.min.css'),
+        new CopyWebpackPlugin([
+            { from: 'src/index.html' },
+            { from: 'src/favicon.ico' }
+        ]),
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true,
+            sourceMap: true
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.min\.css$/,
+            cssProcessorOptions: { discardComments: { removeAll: true } }
+        }),
+        new CleanWebpackPlugin(['dist', 'bin'], {
+            root: '',
+            verbose: true
+        })
     ]
 }
